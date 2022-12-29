feecb2dea48f3dd94e572b1d4d14e977b3087e0d
Jan1si <nikita.evgrafov@inbox.ru>
2022-12-29 15:58:11 +0500

    modified register popup

--- app/src/components/RegisterPopup/index.jsx
+++ app/src/components/RegisterPopup/index.jsx
@@ -5,13 +5,15 @@ import logo from '../../assets/logo.svg';
 
 export const RegisterPopup = () => {
   return (
-    <div className={styles.registerForm}>
-      <div className={styles.headerForm}>
-        <div className={styles.logo}>
-          <img src={logo} alt="Логотип" />
-        </div>
-        <div className={styles.title}>
-          <h3>Регистрация</h3>
+    <div className={styles.popupShadow}>
+      <div className={styles.registerForm}>
+        <div className={styles.headerForm}>
+          <div className={styles.logo}>
+            <img src={logo} alt="Логотип" />
+          </div>
+          <div className={styles.title}>
+            <h3>Регистрация</h3>
+          </div>
         </div>
       </div>
     </div>
